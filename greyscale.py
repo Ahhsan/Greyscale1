10a47d4bda1081e2250fddf1551e32b43cf39b98
Ahhsan <114607041+Ahhsan@users.noreply.github.com>
2023-03-15 17:06:25 +0530

    Update greyscale.py

--- greyscale.py
+++ greyscale.py
@@ -1,11 +1,11 @@
 import cv2
 import matplotlib.pyplot as mp
 
-img = cv2.imread('image.jpg')
+img = cv2.imread('img.jpg')
 img = cv2.resize(img, (1920,1080))
 img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-mp.xlabel("Shade of gray")
-mp.ylabel("NO. of times found in image")
+mp.xlabel("grey")
+mp.ylabel("range")
 mp.hist(img.flatten(),256,[0,255])
-mp.title("Histogram")
+mp.title("greyscale histogram")
 mp.show()
